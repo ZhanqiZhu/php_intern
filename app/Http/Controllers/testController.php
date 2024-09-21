1ce98f4e90861ada3fb344de708f83e758438065
your_username <your_email@example.com>
2024-09-21 17:22:15 +0800

    api测试例子

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class testController extends Controller
+{
+    public function testIndex(Request $request)
+    {
+echo 111;
+
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::group([
+    'namespace' => 'API',
+], function (Router $router) {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    //test
+    $router->get('test/test-index', 'testController@testIndex');
+
 });
